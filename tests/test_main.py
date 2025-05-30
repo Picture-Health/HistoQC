1c21f668397b99dabcf7b9be044bf23ab0ae8d3d
d13g0 <diego.cantor@gmail.com>
2025-05-30 18:05:43 -0400

    updating sckit-image to 0.25.2

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ def test_main():
     try:
         hqc_main(
             basepath='/Users/odin/sw/HistoQC/tests/data',
-            input_pattern='sample3.tiff',
+            input_pattern='standard.tiff',
             outdir='results',
             config='/Users/odin/sw/HistoQC/tests/picture_health/config_ph_v2.1.ini',
         )
